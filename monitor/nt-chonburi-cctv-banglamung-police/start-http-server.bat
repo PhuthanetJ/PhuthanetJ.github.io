ba29a52cb147e668dee29d9d8f664fc81aa07a55
PhuthanetJ <94596542+PhuthanetJ@users.noreply.github.com>
2026-08-06 11:17:17 +0700

    Delete monitor/nt-chonburi-cctv-banglamung-police directory

--- monitor/nt-chonburi-cctv-banglamung-police/start-http-server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM เปิด Dashboard ผ่าน HTTP ที่ Port 8080
-cd /d "%~dp0"
-python -m http.server 8080 --bind 0.0.0.0
-pause
--- monitor/nt-chonburi-cctv-banglamung-police/start-http-server.ps1
+++
@@ -1,3 +0,0 @@
-# เปิด Dashboard ผ่าน HTTP ที่ Port 8080
-Set-Location $PSScriptRoot
-python -m http.server 8080 --bind 0.0.0.0
